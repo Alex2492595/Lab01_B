0c52a79d54893db905251e589b15a3c67ac35e70
Alex2492595 <2492595@edu.vaniercollege.qc.ca>
2025-08-28 23:42:45 -0400

    Fixed second constructor and completed GetBarCode method

--- NetBeansProjects/Lab01/src/main/java/com/mycompany/lab01/ZipCode.java
+++ NetBeansProjects/Lab01/src/main/java/com/mycompany/lab01/ZipCode.java
@@ -31,10 +31,16 @@ public class ZipCode {
     public ZipCode(String barCode) {
         if (barCode.length() != 27) {
             System.out.print("Bar code has incorrect length.");
-        } else if (barCode.charAt(0) != '1') {
-            System.out.print("Bar code missing 1 at beggining.");
-        } else if (barCode.charAt(26) != '1') {
-            System.out.print("Bar code missing 1 at end.");
+        }
+        
+        if (barCode.charAt(0) != '1' || barCode.charAt(26) != '1') {
+            System.out.print("Bar code missing 1 at start/end.");
+        }
+        
+        for (char c : barCode.toCharArray()) {
+            if (c != '0' && c != '1') {
+                System.out.print("Bar code contains bad digit.");
+            }
         }
     }
 
@@ -43,7 +49,26 @@ public class ZipCode {
      * @return the full bar code.
      */
     public String GetBarCode() {
-        return "";
+        String barCode = "1";
+        String zipCode = String.format("%05d", Zip);
+        
+        for (int i = 0; i < 5; i++) {
+            switch (zipCode.charAt(i)) {
+                case 0 -> barCode += "11000";
+                case 1 -> barCode += "00011";
+                case 2 -> barCode += "00101";
+                case 3 -> barCode += "00110";
+                case 4 -> barCode += "01001";
+                case 5 -> barCode += "01010";
+                case 6 -> barCode += "01100";
+                case 7 -> barCode += "10001";
+                case 8 -> barCode += "10010";
+                case 9 -> barCode += "10100";
+            }
+        }
+        
+        barCode += "1";
+        return barCode;
     }
     
     /**
